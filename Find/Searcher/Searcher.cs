99657b9eb40aca3ac3a5b1df36caa6e4f962c6e5
Дима Куликов <dimserk97@gmail.com>
2020-08-19 20:18:27 +0300

    Много всего

---
+++ Find/Searcher/Searcher.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find
+{
+    class Searcher
+    {
+        public bool CaseFlag;
+
+        public Searcher()
+        {
+            this.CaseFlag = false;
+        }
+
+        public void SearchInRange(string what, Range where, ref List<Range> searchResultRanges, ref BindingList<RangeView> searchResultList)
+        {
+            var foundRange = where.Find(what, MatchCase:this.CaseFlag);
+            var worksheetSearchResult = foundRange;
+
+            if (foundRange != null)
+            {
+                var firstAddress = foundRange.Address;
+
+                while (true)
+                {
+                    searchResultList.Add(new RangeView(foundRange));
+
+                    try
+                    { 
+                        foundRange = where.FindNext(foundRange);
+                    }
+                    catch (System.Runtime.InteropServices.COMException)
+                    {
+                        break;
+                    }
+
+                    if (firstAddress != foundRange.Address)
+                    {
+                        worksheetSearchResult = Globals.ThisAddIn.Application.Union(worksheetSearchResult, foundRange);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                searchResultRanges.Add(worksheetSearchResult);
+            }
+        }
+    }
+}
